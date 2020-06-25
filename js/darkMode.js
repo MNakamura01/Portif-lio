71592d238ea73a172536dad8c401e0398ad23c68
mnakamura01 <mrnnakamura01@gmail.com>
2020-06-24 21:31:14 -0300

    botao liga desliga iniciado

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,17 +1,32 @@
 
-
+    let status = 1
 
 
 function mudarModo(modo) {
-    console.log('ok')
-    if (modo == "noturno") {
-        document.getElementById('page-top').style.color = "white"
-        document.getElementById('services').style.background = "rgba(0,0,0,0.85)"
-        document.getElementById('study').style.background = "rgba(0,0,0,0.85)"
-        document.getElementById('extra').style.background = "rgba(0,0,0,0.85)"
-        document.getElementById('portfolio').style.background = "rgba(0,0,0,0.83)"
-        document.getElementById('about').style.background = "rgba(0,0,0,0.83)"
+    console.log(status)
 
+    if (modo == "noturno") {
+        if (status == 0) {
+            status = 1
+            document.getElementById('page-top').style.color = "black"
+            document.getElementById('services').style.background = "white"
+            document.getElementById('study').style.background = "white"
+            document.getElementById('extra').style.background = "white"
+            document.getElementById('portfolio').style.background = "rgb(240, 244, 245)"
+            document.getElementById('about').style.background = "rgb(240, 244, 245)"
+
+            document.getElementById('lamp').img = "src='assets/img/lampada.png'"
+        } else {
+            status = 0
+            document.getElementById('page-top').style.color = "white"
+            document.getElementById('services').style.background = "rgba(0,0,0,0.85)"
+            document.getElementById('study').style.background = "rgba(0,0,0,0.85)"
+            document.getElementById('extra').style.background = "rgba(0,0,0,0.85)"
+            document.getElementById('portfolio').style.background = "rgba(0,0,0,0.83)"
+            document.getElementById('about').style.background = "rgba(0,0,0,0.83)"
+
+            document.getElementById('lamp').img = "src='assets/img/lampadabranca.png'"
+        }
     }
     if (modo == "normal"){
         document.getElementById('page-top').style.color = "black"
